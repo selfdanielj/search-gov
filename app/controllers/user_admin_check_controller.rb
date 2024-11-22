c0b14b05b918e72ef80ad1e2d0652de6f87a1da2
selfdanielj <38377823+selfdanielj@users.noreply.github.com>
2024-11-22 14:25:34 -0500

    SRCH-5708 rubocop updates

--- app/controllers/user_admin_check_controller.rb
+++ app/controllers/user_admin_check_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserAdminCheckController < ApplicationController
-  # Performs a admin check on a given email address.  `Returns 'is_admin': true` for 
+  # Performs a admin check on a given email address.  `Returns 'is_admin': true` for
   # if a match on email is found and the user is an affiliate admin.
 
   skip_before_action :verify_authenticity_token
@@ -9,15 +9,16 @@ class UserAdminCheckController < ApplicationController
 
   def admin_check
     email = admin_check_params[:email]
-    if email.nil? or email.blank?
+    if email.nil? || email.blank?
       render json: { error: "An 'email' parameter and value are required." }, status: :unprocessable_entity
     else
-      user = User.find_by email: email, is_affiliate_admin: true
-      render json: {email: email, is_admin: user.nil? ? false : true}, status: :ok
+      user = User.find_by(email:, is_affiliate_admin: true)
+      render json: { email:, is_admin: user ? false : true }, status: :ok
     end
   end
-  
+
   private
+
   def admin_check_params
     params.permit(:email)
   end
